514302176919525ca86397b3a9cd1a83d11adfce
Samuele Santi <redshadow@hackzine.org>
2018-06-29 13:43:47 +0100

    Customizations
    - Support for loading SCSS (modules)
    - Configure eslint
    - Load modules from ./src
    - Add _redirects (netlify) and robots.txt

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,14 +1,68 @@
 module.exports = {
-  use: [
-    '@neutrinojs/standardjs',
-    [
-      '@neutrinojs/react',
-      {
-        html: {
-          title: 'react-neutrino-starter-rshk'
-        }
-      }
-    ],
-    '@neutrinojs/jest'
-  ]
+    use: [
+        '@neutrinojs/standardjs',
+        ['@neutrinojs/react', {
+            devServer: {
+                host: '127.0.0.1',
+                port: 3000,
+                https: false,
+            },
+            html: {
+                title: 'react-neutrino-starter-rshk'
+            }
+        }],
+        '@neutrinojs/jest',
+        ['@neutrinojs/eslint', {
+            eslint: {
+                plugins: ['import', 'flowtype', 'jsx-a11y', 'react'],
+                rules: {},
+                baseConfig: {extends: ['eslint-config-react-app']},
+            },
+        }],
+        ['@neutrinojs/style-loader', {
+            test: /\.global\.(css|sass|scss)$/,
+            modulesTest: /(?<!\.global)\.(css|sass|scss)$/,
+            modules: true,
+            css: {
+                localIdentName: '[local]--[hash:base64:8]',
+            },
+            loaders: [
+                {
+                    loader: 'sass-loader',
+                    useId: 'sass',
+                    options: {
+                        includePaths: ['node_modules', 'src'],
+                        localIdentName: '[local]--[hash:base64:8]',
+                    }
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [
+                            require('autoprefixer')({
+                                browsers: [
+                                    '>1%',
+                                    'last 4 versions',
+                                    'Firefox ESR',
+                                    'not ie < 9', // React doesn't support IE8 anyway
+                                ],
+                                flexbox: 'no-2009',
+                            }),
+                        ]
+                    }
+                }
+            ]
+        }],
+        (neutrino) => {
+            neutrino.config.resolve
+                    .modules
+                    .add(neutrino.options.source);
+        },
+        ['@neutrinojs/copy', {
+            patterns: [
+                {from: './src/static', to: './'},
+            ],
+            options: {},
+            pluginId: 'copy'}],
+    ]
 };
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './App.scss';
+
+
+export default class App extends React.Component {
+  state = {
+    name: 'react-neutrino-starter-rshk'
+  };
+
+  render () {
+    return (
+        <div className="container">
+            <div className={styles.App}>
+                <h1 className={styles.Title}>
+                    Welcome to {this.state.name}
+                </h1>
+            </div>
+            <button type="button" className="btn btn-primary">
+                Themed button
+            </button>
+        </div>
+    )
+  }
+}
